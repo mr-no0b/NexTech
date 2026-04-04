e9f834a1d57d1fb3c2279047b0f5b3510490fc43
mr-no0b <raihanarefin.cse@gmail.com>
2026-04-04 15:34:32 +0600

    initialize Spring Boot project with pom.xml and config

---
+++ src/main/java/com/nextech/NexTechApplication.java
@@ -0,0 +1,11 @@
+package com.nextech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexTechApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NexTechApplication.class, args);
+    }
+}
